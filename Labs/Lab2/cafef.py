27da9315da6271ca8b5c7305ea7e2794e309c536
unknown <you@example.com>
2018-08-04 10:20:34 +0700

    mm

--- Labs/Lab2/cafef.py
+++ Labs/Lab2/cafef.py
@@ -22,12 +22,15 @@ for tr in tr_list:
     td_list = tr.find_all("td")
     data = {}
     if td_list and td_list[0].string is not None:
-        data['title'] = (td_list[0].string)
+        data['title'] = (td_list[0].string.strip())
         data['4-2017'] = (td_list[1].string)
         data['1-2018'] = (td_list[2].string)
         data['2-2018'] = (td_list[3].string)
         data['3-2018'] = (td_list[4].string)
-    datas.append(data)
+    if data:
+        # data != [] => TRUE
+        # data == [] => FALSE
+        datas.append(data)
     
 pyexcel.save_as(records = datas, dest_file_name = "cafef.xlsx")
 
